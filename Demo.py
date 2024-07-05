4a9ad196901dac399633cbfcfd1f31a798e61563
Batsirai Nyakotyo <batsinyakotyo@gmail.com>
2024-07-05 17:12:36 +0000

    second

--- Demo.py
+++ Demo.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect,request
 from forms import RegistrationForm
 from flask_behind_proxy import FlaskBehindProxy
 
@@ -7,10 +7,11 @@ proxied = FlaskBehindProxy(app)
 app.config['SECRET_KEY'] = '8c6ae31babf5d324c2092ba94d8f3df5'
 
 @app.route("/")
-def hello_world():
+@app.route("/home")
+def home():
     return render_template('home.html', subtitle='Home Page', text='This is the home page')
     
-@app.route("/second_page")
+@app.route("/second_page.html")
 def second_page():
     return render_template('second_page.html', subtitle='Second Page', text='This is the second page')
 
@@ -21,9 +22,16 @@ def register():
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home')) # if so - send to home page
     return render_template('register.html', title='Register', form=form)
-    
+
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/CHANGE_TO_PYTHON_ANYWHERE_USERNAME/CHANGE_TO_GITHUB_REPO_NAME')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
-
-
-    
